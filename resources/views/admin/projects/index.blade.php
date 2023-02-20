a2a2cabcf16615af8e8f4ebd4a1c2f38ef10d396
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-20 18:47:08 +0100

    add table structure

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,26 +2,39 @@
 
 @section('content')
     <div class="container">
-        <table class="table table-hover table-borderless m-5">
+        <table class="table table-hover table-borderless mt-5">
             <thead>
                 <tr>
                     <th scope="col">#id</th>
                     <th scope="col">Title</th>
                     <th scope="col">used_language</th>
                     <th scope="col">
-                        <a href="" class="btn btn-primary">
+                        <a href="" class="btn btn-info">
                             Create a new project
                         </a>
                     </th>
                 </tr>
             </thead>
             <tbody>
+                @foreach ($projects as $project)
                 <tr>
-                    <th scope="row"></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->used_language }}</td>
+                    <td>
+                        <a href="" class="btn btn-sm btn-primary">
+                            Show
+                        </a>
+                        <a href="" class="btn btn-sm btn-success">
+                            Edit
+                        </a>
+                        <a href="" class="btn btn-sm btn-danger">
+                            Delete
+                            {{-- TODO: fare FROM per il btn danger --}}
+                        </a>
+                    </td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
