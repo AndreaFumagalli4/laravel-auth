8d06380d6589ef5339166b809c57fc9d7af0430c
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-20 18:12:34 +0100

    create index.blade.php and start table structure

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <table class="table table-hover table-borderless m-5">
+            <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">used_language</th>
+                    <th scope="col">
+                        <a href="" class="btn btn-primary">
+                            Create a new project
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row"></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
