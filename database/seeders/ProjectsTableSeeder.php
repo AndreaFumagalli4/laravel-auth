62a923e0248c170f36e0177e703ea10c22668934
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-22 18:00:08 +0100

    add migration update table

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectsTableSeeder extends Seeder
 {
@@ -18,10 +19,12 @@ class ProjectsTableSeeder extends Seeder
     {
         for ($i=0; $i < 50; $i++) { 
             $newProject = new Project();
-            $newProject->title = $faker->sentence(5);
+            $newProject->title = $faker->unique()->sentence(5);
+            $newProject->slug = Str::slug($newProject->title);
             $newProject->thumb = $faker->imageUrl(360, 360, 'computers', true);
             $newProject->used_language = $faker->word();
             $newProject->link = $faker->url();
+            $newProject->project_date = $faker->dateTimeThisYear();
             $newProject->save();
         }
     }
